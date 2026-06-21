dff2331364fa57a77b35477a24b4a3c20f06d563
bellround <rainarmy11@gmail.com>
2026-06-21 23:40:37 +0900

    login업데이트

--- login/login.js
+++ login/login.js
@@ -8,6 +8,8 @@ document.querySelector('#close-btn').addEventListener('click', function(){
 
 const pwInput = document.querySelector('#password')
 
+const idInput = document.querySelector('#email')
+
 const pwSt = document.querySelector('#pw-status')
 
 const pwBar = document.querySelector('.pw-gauge-bar')
@@ -16,8 +18,41 @@ document.querySelector('#login-btn').addEventListener('click', function(){
     document.querySelector('.black-bg').classList.remove('show')
 })
 
+
 document.querySelector('#signup-btn').addEventListener('click', function(){
-    alert(`회원가입에 완료되었습니다. 로그인버튼을 누르세요.`);
+    const len = pwInput.value.length
+    const ad_len = idInput.value.length
+
+    if (len < 6)
+    {
+        alert(`비밀번호를 6글자 이상 입력해주세요.`)
+        return
+    }
+    if (ad_len < 4)
+    {
+        alert(`아이디를 4글자 이상 입력해주세요.`)
+        return
+    }
+    alert(`회원가입에 완료되었습니다. 로그인버튼을 누르세요.`)
+
+})
+
+document.querySelector('#login-btn').addEventListener('click', function(){
+    const len = pwInput.value.length
+    const ad_len = idInput.value.length
+
+    if (len < 6)
+    {
+        alert(`비밀번호나 아이디가 다릅니다`)
+        return
+    }
+    if (ad_len < 4)
+    {
+        alert(`비밀번호나 아이디가 다릅니다`)
+        return
+    }
+    document.location.href = "http://127.0.0.1:5500/main/main.html";
+
 })
 
 pwInput.addEventListener('input', function(){
